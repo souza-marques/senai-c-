17445035c70436dead80503160d5518079598e99
Matheus Marques <marquesmatheus018@gmail.com>
2019-09-19 11:23:53 -0300

    inicio csharp

---
+++ Calculadora2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Calculadora2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            string oper;
+
+            Console.WriteLine("Digite o  1º Número:");
+            num1 = int.Parse(Console.ReadLine ());
+            Console.WriteLine("Digite o 2º Número:");
+            num2 = int.Parse(Console.ReadLine ());
+            Console.WriteLine("Qual o operador desejado?");
+            Console.WriteLine(" + = Somar");
+            Console.WriteLine(" - = Subtrair");
+            Console.WriteLine(" * = Multiplicar");
+            Console.WriteLine("/ = Dividir ");
+            Console.WriteLine(" % = Porcentagem");
+            oper = Console.ReadLine();
+
+            switch(oper) {
+                  case "+":
+                  Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                  break;
+                  case "-":
+                  Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                  break;
+                  case "*":
+                  Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                  break;
+                  case "/":
+                  Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+                  break;
+                  case "%":
+                  Console.WriteLine($"{num1} % {num2} = {num1 % num2}");
+                  break;
+            }
+        
+        }
+    }
+}
---
+++ Idade/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int idade = 0;
+           Console.WriteLine("Qual a sua idade em anos?");
+           idade = int.Parse(Console.ReadLine());
+        
+           Console.WriteLine("sua idade em dias é: " + idade * 365);
+
+           Console.WriteLine("sua idade em meses é: " + idade * 12);
+
+           Console.WriteLine("sua idade em horas é: " + idade * 8760);
+
+           Console.WriteLine("sua idade em minutos é: " + idade * 525600);
+    }
+}
+       
+}
+
