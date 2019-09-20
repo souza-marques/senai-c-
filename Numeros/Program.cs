c8cbdf60070c37326123d1337a81d909acab059f
Matheus Marques <marquesmatheus018@gmail.com>
2019-09-20 11:21:47 -0300

    aula dia 20/09/2019

---
+++ Numeros/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+           
+            int numero = 0;
+            
+            do {
+            Console.WriteLine("Digite um numero inteiro");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero == 0){
+                Console.WriteLine(" Fim do programa");
+            } else
+            
+             if (numero % 2 == 0) {
+            Console.WriteLine("Seu número é par");
+              }
+            else {
+            Console.WriteLine(" Seu número é ímpar");
+            }
+            
+            } while (numero !=0);
+        }
+    }
+}
---
+++ tabuada_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tabuada_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada do 5");
+            for (int i = 5; i <= 50 ; i+=5){
+                Console.WriteLine(i + " " );
+            
+
+            }
+        }
+    }
+}
