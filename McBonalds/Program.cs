54559124f327beff29af74daf3853c1dca654e69
Matheus Marques <marquesmatheus018@gmail.com>
2019-09-27 11:20:56 -0300

    aulaPOO

---
+++ McBonalds/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace McBonalds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        cliente cliente1 = new cliente ("Alexandre", "55 11 5555-5555","alexandre@gmail.com");
+        Console.WriteLine("Nome: " + cliente1.Nome);
+        Console.WriteLine("Telefone: " + cliente1.Telefone);
+        Console.WriteLine("Email: " + cliente1.Email);
+          }
+    }
+}
